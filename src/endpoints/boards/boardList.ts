f855a3130f9a27848980787457f26eb41cde5f5d
Liz MacLean <18120837+lizziemac@users.noreply.github.com>
2026-02-03 12:50:48 -0500

    Stupid autofill

--- src/endpoints/boards/boardList.ts
+++ src/endpoints/boards/boardList.ts
@@ -1,8 +1,6 @@
 import { D1ListEndpoint, RequestTypes, ResponseConfig } from "chanfana";
 import { HandleArgs } from "../../types";
 import { BoardModel } from "./base";
-import { ExternalDocumentationObject, ParameterObject, ReferenceObject, RequestBodyObject, CallbacksObject, SecurityRequirementObject, ServerObject } from "openapi3-ts/oas30";
-import { ExternalDocumentationObject, ParameterObject, ReferenceObject, RequestBodyObject, CallbacksObject, SecurityRequirementObject } from "openapi3-ts/oas31";
 
 export class BoardList extends D1ListEndpoint<HandleArgs> {
   _meta = {
